c2129245fbe944af6997316617a7a35d0e2044ed
Anton Emin <anton.emin.work@gmail.com>
2019-12-24 23:01:41 +0200

    eslint error fix

--- FL12_HW6/homework/js/task1.js
+++ FL12_HW6/homework/js/task1.js
@@ -1,4 +1,4 @@
-const four = 4, two = 2;
+const four = 4, two = 2, ten = 10;
 let x1 = 0, x2 = 0;
 let a = Number.parseInt(prompt('Enter your a for quadratic equation (ax2 + bx + c = 0).'));
 let b = Number.parseInt(prompt('Enter your b for quadratic equation (ax2 + bx + c = 0).'));
@@ -6,12 +6,12 @@ let c = Number.parseInt(prompt('Enter your c for quadratic equation (ax2 + bx + 
 if (isNaN(a)||isNaN(b)||isNaN(c)) {
     alert('Invalid input data!!!');
 } else {
-    let disc = Math.pow(b,2) - four*a*c;
+    let disc = Math.pow(b, two) - four * a * c;
     if (disc > 0) {
-        x1 = Math.ceil(((-b + Math.sqrt(disc)) / (two * a))*10)/10;
-        x2 = Math.ceil(((-b - Math.sqrt(disc)) / (two * a))*10)/10;
+        x1 = (-b + Math.sqrt(disc)) / (two * a);
+        x2 = (-b - Math.sqrt(disc)) / (two * a);
         console.log('x1 = ' + x1 + ' x2 = ' + x2)
-    } else if (disc === 0) {9
+    } else if (disc === 0) {
         x1 = -b / (two * a) ;
         console.log('x1 =' + x1)
     } else {
