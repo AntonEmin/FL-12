b00b7871d2e3fa7a8a4cac2f958c69e76ac71563
Anton Emin <anton.emin.work@gmail.com>
2019-12-31 16:45:27 +0200

    Fix countNumbers

--- FL12_HW8/homework/js/countNumbers.js
+++ FL12_HW8/homework/js/countNumbers.js
@@ -1,27 +1,19 @@
 function makeNumber (string) {
-    let number ='';
-    string.split('').forEach(element => {         
-        if (!isNaN(parseInt(element,10))) {
-            number += element;
-        }
-    })    
-return number;
+  let number ='';
+  string.split('').forEach(element => {         
+    if (!isNaN(parseInt(element,10))) {
+      number += element;
+    }
+  })    
+  return number;
 }
 function countNumbers (string) {
-    let numbers = makeNumber(string).split('');
-    let uniqNumbers = [],
-        countUniqNumbers = [];
-    
-    for (let temp of numbers) {
-        if(!uniqNumbers.includes(temp)){
-            uniqNumbers.push(temp);
-        }
-    }
-    for (let i = 0; i < uniqNumbers.length;i++) {
-        for (let temp of numbers) {
-            if (uniqNumbers[i] === temp) {
-                countUniqNumbers[i] += 1;
-            }
-        }
-    }
+  let numbers = makeNumber(string).split('');
+  let ret = {};
+  let len = numbers.length;
+  while (len--) {
+    ret[numbers[len]] = ret[numbers[len]] + 1 || 1;
+  }
+  return ret;    
 }
+countNumbers('da4d5ad54a6d4ada6wd89aw7da89as');
