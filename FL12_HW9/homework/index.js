3f983d740715864b5091dd04f58ee4d5737d721d
Anton Emin <anton.emin.work@gmail.com>
2020-01-14 23:58:30 +0200

    fix spacing

--- FL12_HW9/homework/index.js
+++ FL12_HW9/homework/index.js
@@ -1,81 +1,79 @@
 const actors = [
-    { name: 'tommy', age: 36 },
-    { name: 'lee', age: 28 }
-  ];
-const date = new Date(2019, 0, 2);  
-
+  { name: 'tommy', age: 36 },
+  { name: 'lee', age: 28 }
+],
+ date = new Date(2019, 0, 2);  
 function convert(...theArgs) {
-    let result = [];
-    for (let i = 0; i < theArgs.length; i++){
-        if (typeof theArgs[i] === 'number') {
-            result.push(theArgs[i]+'');
-       } else {
-        result.push(+ theArgs[i]);
-       }
+  let result = [];
+  for (let i = 0; i < theArgs.length; i++){
+    if (typeof theArgs[i] === 'number') {
+      result.push(theArgs[i]+'');
+    } else {
+    result.push(+ theArgs[i]);
     }
-    
-    return result;
+  }
+  return result;
 }
 
 function executeforEach (array, callback) {    
-    for (let i = 0; i < array.length; i++) {
-        callback(array[i]);
-    }
+  for (let i = 0; i < array.length; i++) {
+    callback(array[i]);
+  }
 }
 
 function mapArray (array, callback) {
-    let result = [];
-    executeforEach(array, function(el) {
-        if (typeof el === 'string') {
-            el = + el;
-        }
-        result.push(callback(el));
-    });   
-    return result;
+  let result = [];
+  executeforEach(array, function(el) {
+    if (typeof el === 'string') {
+      el = + el;
+    }
+  result.push(callback(el));
+});   
+return result;
 }
 
 function filterArray (array, callback) {
-    let result = [];
-    executeforEach(array, function(el) {
-        if (callback(el)) {
-            result.push(el);
-        }        
-    });   
-    return result;
+  let result = [];
+  executeforEach(array, function(el) {
+    if (callback(el)) {
+      result.push(el);
+    }        
+  });   
+  return result;
 }
 
 function flipOver (str) {
-    let flipedStr = '';
-    for (let character of str) {
-        flipedStr = character + flipedStr;
-    }
-    return flipedStr;
+  let flipedStr = '';
+  for (let character of str) {
+    flipedStr = character + flipedStr;
+  }
+  return flipedStr;
 }
 
 function makeListFromRange (array) {
-    let result = [];
-    for (let i = array[0]; i < array[1]+1;i++) {
-        result.push(i);
-    }
-    return result;
+  let result = [];
+  for (let i = array[0]; i < array[1]+1;i++) {
+    result.push(i);
+  }
+  return result;
 }
 
 function getArrayOfKeys (actors, key) {
-    let result = [];
-    executeforEach(actors, function(el) {
-        result.push(el[key]); 
-    })
-    return result;
+  let result = [];
+  executeforEach(actors, function(el) {
+    result.push(el[key]); 
+  })
+  return result;
 }
 
 function substitute (array) {
-    return mapArray(array, function(el) {
-        if (el < 30) {
-            el = '*';
-        }
-    })
+  return mapArray(array, function(el) {
+    if (el < 30) {
+      el = '*';
+    }
+  })
 }
 
 function getPastDay (date, days) {
-    return new Date(new Date().setDate(date.getDate() - days));
+  return new Date(new Date().setDate(date.getDate() - days));
 }
